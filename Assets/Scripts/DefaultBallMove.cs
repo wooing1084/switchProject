ba5744e3766d5b7c82680f03ce0f78e27097ddba
정성훈 <kmjung1515@naver.com>
2018-08-06 21:46:50 +0900

    볼 움직임 및 장애물 이동

---
+++ Assets/Scripts/DefaultBallMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultBallMove : MonoBehaviour {
+    public float moveSpeed;                         //초기 움직임 속도
+    Vector2 moveDir;                         //움직일 방향 벡터
+    public float moveAccel;                         //가속도
+    public float initSpeed;
+
+    // Use this for initialization
+    void Start () {
+        moveSpeed = initSpeed = 1.5f;
+        moveDir.x = 0; moveDir.y = 1;
+        moveAccel =  0.1f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //키입력
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            moveDir *= -1;               //방향전환
+            moveSpeed *= 0.8f;       //가속도 증가
+            if (moveSpeed < initSpeed)
+                moveSpeed = initSpeed;
+        }
+
+        //방향과 가속도를이용해 이동
+        this.transform.Translate(moveDir * (moveSpeed) * Time.deltaTime, Space.World);
+        moveSpeed += moveAccel;
+
+       	
+	}
+}
---
+++ Assets/Scripts/DefaultHuddleMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultHuddleMove : MonoBehaviour {
+    //장애물의 X좌표가 -4를 넘으면 화면 밖으로 나감
+    //장애물 부모의 Y좌표는 랜덤값을 만들때 -1 ~ -6으로 만들어야 탈출구멍이 화면안에 나옴
+    //허들들 사이의 간격을 3
+
+
+    public Vector2 moveDir = new Vector2(-1,0);
+    public float moveAccel = 2.0f;
+    public GameObject nextHuddle;                   //다음장애물
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(moveDir * Time.deltaTime * moveAccel,Space.World);
+        //맵밖으로 나가면 포지션 재설정
+        if(this.transform.position.x <= -4)
+        {
+            Vector3 pos = this.transform.position;
+            pos.x = nextHuddle.transform.position.x + 3;
+            pos.y = Random.Range(0.0f, -5.0f);
+            this.transform.position = pos;
+
+        }
+	}
+}
