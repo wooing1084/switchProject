30399cc1ed03c9714c9fe5e67065dd198141b65a
정성훈 <kmjung1515@naver.com>
2018-09-29 18:39:50 +0900

    패턴 추가 및 스코어 적용

---
+++ Assets/Scripts/WideHuddlePatern.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WideHuddlePatern : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetHeight(GameObject beforeObject, float range)
+    {
+        Vector3 pos = this.transform.position;
+        float min = beforeObject.transform.position.y - range;
+        float max = beforeObject.transform.position.y + range;
+        if (min < -5.0)
+            min = -5.0f;
+        if (max > 0.0f)
+            max = 0.0f;
+        pos.y = Random.Range(min, max);
+        this.transform.position = pos;
+    }
+
+    public void SetWidth(GameObject me, float widthRange)
+    {
+        Vector3 scaleVec = this.transform.localScale;
+        float min = 1;
+        float max = 1 + widthRange;
+        if (max > 5.0f)
+            max = 5.0f;
+        scaleVec.x = Random.Range(min, max);
+        scaleVec.y = 6;
+        scaleVec.z = 1;
+       // me.
+        me.transform.localScale = scaleVec;
+    }
+}
---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour {
+
+    private int score = 0;          //점수
+    public static gameManager instance;
+    public Text scoreText;
+
+    private void Awake()
+    {
+        if (!instance)
+            instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //점수추가
+    public void AddScore(int val)
+    {
+        score += val;
+        scoreText.text = "Score: " + score;
+    }
+}
