f6cdcbc76dd5138dbce9be5d81fd1c1dc4440479
정성훈 <kmjung1515@naver.com>
2018-10-31 22:06:27 +0900

    태민 코드 수정

--- Assets/CoinFst.cs
+++ Assets/CoinFst.cs
@@ -2,48 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinFst : MonoBehaviour {
+public class CoinFst : MonoBehaviour
+{
 
     private SpriteRenderer spriterenderer;
     public Sprite[] sprites = new Sprite[10];
     /*int sum1 = GlobalVariable.coin;   
     int sum2 = GlobalVariable.coin;
     int sum3 = GlobalVariable.coin;*/
-    int sum1 = 12;   
-    int sum2 = 12;
-    int sum3 = 12;
-    int s1, s2, s3;
+    int coin = GlobalVariable.coin;
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         spriterenderer = GetComponent<SpriteRenderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        s1 = sum1 / 100;
-        s2 = sum2 / 10;
-        s3 = sum3 / 1;
-		if(s1 == 0)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //나누는 수
+        int divNum = 10;
+        //현재 수
+        int nowNum;
+
+        while (coin % divNum != coin)
         {
-            if(s2 == 0)
-            {
-                if(s3 == 0)
-                {
-                    spriterenderer.sprite = sprites[0];
-                }
-                else
-                {
-                    spriterenderer.sprite = sprites[s3];
-                }
-            }
-            else
-            {
-                spriterenderer.sprite = sprites[s2];
-            }
+            //n자리수까지 자름
+            nowNum = coin % divNum;
+            //n자리수 숫자 구하기 ex) nowNum = 350 divNum = 1000일때 저 공식을 사용하면 350 / 100 이기 때문에 3이 된다
+            nowNum /= (divNum / 10);
         }
-        else
-        {
-            spriterenderer.sprite = sprites[s1];
-        }
-	}
+
+    }
 }
