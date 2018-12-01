996dda0585083bfc9913596e0b22592fd56eda8a
DESKTOP-AT90UD1\SAMSUNG <jinkitea@naver.com>
2018-12-01 20:18:22 +0900

    진기태 메인게임 적용 ^^

--- Assets/Scripts/SeongHoon/DefaultCollision.cs
+++ Assets/Scripts/SeongHoon/DefaultCollision.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DefaultCollision : MonoBehaviour {
-   
+public class DefaultCollision : MonoBehaviour
+{
+
     public ParticleSystem Blueblast;
-    
+
     GameObject ball;
 
-	// Use this for initialization
-	void Start () {
-    
+
+    public int Getcomponent { get; private set; }
+    public int Sphere { get; private set; }
+
+    // Use this for initialization
+    void Start()
+    {
+
         ball = GameObject.FindGameObjectWithTag("Player");
     }
 
@@ -22,11 +28,14 @@ public class DefaultCollision : MonoBehaviour {
             UnityEngine.Debug.Log("벽과 충돌");
 
             //게임오버 씬 전환부분
-            ball.SetActive(false);
+
+            Blueblast.transform.position = ball.transform.position;
+            //볼이 장애물의 위치에 닿았을 때, 닿은 위치를 파티클 위치에 담는다.
             Blueblast.Play();
+
             ball.SetActive(false);
             //공 사라짐
-            
+
 
 
         }
@@ -41,6 +50,7 @@ public class DefaultCollision : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
-	}
+    void Update()
+    {
+    }
 }
