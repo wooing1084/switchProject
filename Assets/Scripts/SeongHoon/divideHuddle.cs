242c960cac0df9ca2bb23613d9458e94e06f7435
kmjung1515 <kmjung1515@naver.com>
2018-12-04 18:50:06 +0900

    추가

---
+++ Assets/Scripts/SeongHoon/divideHuddle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class divideHuddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void divide(GameObject stdHuddle, GameObject dividedHuddle,float maxDis,float minDis,bool stdIsParent)
+    {
+        float dis = Random.Range(minDis, maxDis);
+        if (stdIsParent)
+        {            
+            Vector3 pos = dividedHuddle.transform.position;
+            pos.x = stdHuddle.transform.position.x + dis;
+            dividedHuddle.transform.position = pos;
+        }
+        else
+        {
+            Vector3 pos = stdHuddle.transform.localPosition;
+            pos.x -= dis;
+            stdHuddle.transform.localPosition = pos;
+            pos = dividedHuddle.transform.position;
+            pos.x += dis;
+            dividedHuddle.transform.position = pos;
+        }
+    }
+}
